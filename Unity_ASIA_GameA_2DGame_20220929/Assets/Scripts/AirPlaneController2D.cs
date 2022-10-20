7ecd6a055606404d7d07a75872e58c3f0e629f57
KID <50666207+KID421@users.noreply.github.com>
2022-10-20 18:52:48 +0800

    Update 移動與控制系統

---
+++ Unity_ASIA_GameA_2DGame_20220929/Assets/Scripts/AirPlaneController2D.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace KID
+{
+    /// <summary>
+    /// 2D 飛機控制器
+    /// </summary>
+    public class AirPlaneController2D : MonoBehaviour
+    {
+        [Header("移動速度")]
+        [SerializeField, Range(0, 10)]
+        private float speedVertical = 3.5f;
+        [SerializeField, Range(0, 10)]
+        private float speedHorizontal = 4f;
+        [Header("圖片")]
+        [SerializeField] private Sprite pictureUp;
+        [SerializeField] private Sprite pictureMiddle;
+        [SerializeField] private Sprite pictureDown;
+    }
+}
+
