8b678669ef5f6aeb622e8c2d586030b8db1d4229
KID <50666207+KID421@users.noreply.github.com>
2022-11-03 18:59:29 +0800

    Update 完成傷害系統並轉換為網頁版

--- Unity_ASIA_GameA_2DGame_20220929/Assets/Scripts/FireSystemBase.cs
+++ Unity_ASIA_GameA_2DGame_20220929/Assets/Scripts/FireSystemBase.cs
@@ -11,5 +11,15 @@ namespace KID
         private GameObject prefabBullet;
         [SerializeField, Header("子彈生成點")]
         private Transform pointSpawn;
+
+        // 自訂方法
+        // 生成子彈
+        // 保護級別：允許子類別存取
+        protected void SpawnBullet()
+        {
+            // 實例化(生成物件，座標，角度)；
+            // 生成 子彈預製物 座標與角度跟生成點相同
+            Instantiate(prefabBullet, pointSpawn.position, pointSpawn.rotation);
+        }
     }
 }
